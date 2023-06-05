18fc6212782ba9b64053f429a5910228c32c2cd3
poojakasar <96623437+poojabalukasar@users.noreply.github.com>
2023-06-05 11:35:30 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:5000/api";
+export const server = "http://127.0.0.1:5000/api";
 
-export const backend_url = "http://localhost:5000/";
+export const backend_url = "http://127.0.0.1:5000/";
